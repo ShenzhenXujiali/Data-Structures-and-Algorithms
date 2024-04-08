f29da0f73498120aed441640800978968a15362a
shenzhenxujiali <xujiali04@163.com>
2024-04-08 23:39:24 +0800

    update

---
+++ algorithm_code/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import main.java.algorithms.sort.QuickSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arrayToSort = { 9, -3, 5, 2, 6, 8, -6, 1, 3 };
+
+        QuickSort quickSorter = new QuickSort();
+        quickSorter.quickSort(arrayToSort, 0, arrayToSort.length - 1);
+
+        // Print out the sorted array
+        for (int i : arrayToSort) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
